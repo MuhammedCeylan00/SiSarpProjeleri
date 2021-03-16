0c4a329d67516deeb657470c33fe41d8e7b2fdf7
Muhammed ceylan <72267252+MuhammedCeylan00@users.noreply.github.com>
2021-03-17 00:04:09 +0300

    Add files via upload

---
+++ SqlOrnek/SqlOrnek/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace SqlOrnek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(51, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(734, 553);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ad Soyad";
+            this.columnHeader1.Width = 154;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Şehir";
+            this.columnHeader2.Width = 138;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Okul";
+            this.columnHeader3.Width = 116;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(319, 637);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GÖRÜNTÜLE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Kulüp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1169, 731);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ SqlOrnek/SqlOrnek/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SqlOrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Öğrenciler;Integrated Security=True");//bağlantı oluşturuldu.
+        private void VerilerimiGoster()
+        {
+            baglan.Open();//liste açıldı.
+            SqlCommand komut = new SqlCommand("select *from Bilgiler",baglan);//listeleme işlemi sql komutu
+            SqlDataReader oku = komut.ExecuteReader();//okuma işlemi
+
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem(); //listeye ekleme işlemi
+                ekle.Text = oku["Ad Soyad"].ToString();
+                ekle.SubItems.Add(oku["Şehir"].ToString());
+                ekle.SubItems.Add(oku["Okul"].ToString());
+                ekle.SubItems.Add(oku["Kulup"].ToString());
+                listView1.Items.Add(ekle); //ekrana verilmesi
+            }
+            baglan.Close();//açılan listenin kapatılması.
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VerilerimiGoster();//butona tıklandığında verilerimiGoster fonksiyonu açıldı.
+        }
+    }
+}
